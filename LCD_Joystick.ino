347d87c88e62ca6f63e662e850f8133c23623c54
Liam <35880755+LargeFoot@users.noreply.github.com>
2021-02-14 17:43:01 +0000

    Update LCD_Joystick.ino

--- LCD_Joystick.ino
+++ LCD_Joystick.ino
@@ -1,3 +1,5 @@
+/*(C) Liam Letters 2021
+Free use for whatever purpose is permitted, provide this copyright comment/segment remains intact*/
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(2,3,4,5,6,7);
